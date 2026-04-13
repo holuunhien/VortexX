6b55382477625182b4aed170d2d3cf2255b13e8d
LNX <thilienluu.011991@gmail.com>
2026-04-13 10:57:10 +0700

    Delete .github/workflows/lt.py

--- .github/workflows/lt.py
+++
@@ -1,5 +0,0 @@
-# Viết chương trình nhập họ tên đầy đủ của người dùng, sau đó in thông báo tên và họ đệm của người đó.
-s=input("Nhập họ tên: ")
-A=s.split(" ")
-print("Tên: ", A[len(A)-1])
-print("Tên đệm: ", A[len(A)-2])
