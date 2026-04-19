b00093ef45154b78bb7a7b1673a8588c6560ed5c
LNX <thilienluu.011991@gmail.com>
2026-04-19 16:27:51 +0700

    Delete .github/workflows/luyen tap 1.py

--- .github/workflows/luyen tap 1.py
+++
@@ -1,7 +0,0 @@
-# Thiết lập hàm power(a,b,c) với a, b, c là số nguyên. Hàm trả lại giá trị (a+b)c
-a = int(input("nhap a: "))
-b = int(input("nhap b: "))
-c = int(input("nhap c: "))
-def power(a, b, c):
-    return (a + b) ** c
-print(power(a, b, c))
