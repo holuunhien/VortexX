081909792eb9d70c52ba151bd07013ecd75a45b8
LNX <thilienluu.011991@gmail.com>
2026-04-19 16:27:42 +0700

    Delete .github/workflows/luyen tap 2.py

--- .github/workflows/luyen tap 2.py
+++
@@ -1,4 +0,0 @@
-# Viết chương trình thực hiện: Nhập hai số tự nhiên từ bàn phím, hai số cách nhau bởi dấu cách. Tính và in ra tổng của các số này.
-a, b = map(int, input("Nhập hai số tự nhiên cách nhau bởi dấu cách: ").split(" "))
-tong = a + b
-print("Tổng của hai số là:", tong)
