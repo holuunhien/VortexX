bbe050990aad1d6eab850e8a8931eb5169c8b90d
LNX <thilienluu.011991@gmail.com>
2026-05-05 10:25:48 +0700

    Delete .github/workflows/b3.py

--- .github/workflows/b3.py
+++
@@ -1,8 +0,0 @@
-def so_chia_het_cho_5(n):
-    dem = 0
-    for i in range (n):
-        if i % 5 == 0:
-            dem = dem + 1
-    return dem
-n = int(input("nhap so tu nhien n: "))
-print("so nho hon n chia het cho 5:", so_chia_het_cho_5(n))
